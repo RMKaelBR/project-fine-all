38dd2ee531652bd4fc3795e56736f6312646575e
RMKaelBR <rmbresurreccion@gmail.com>
2023-10-04 21:42:38 +0800

    add searching function powered by ransack

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
     email
   end
 
+  def username
+    self.email.split('@').first
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["confirmed_at", "created_at", "current_sign_in_at", "current_sign_in_ip", "email", "id", "last_sign_in_at", "last_sign_in_ip", "remember_created_at", "reset_password_sent_at", "reset_password_token", "sign_in_count", "unconfirmed_email", "updated_at"]
   end
