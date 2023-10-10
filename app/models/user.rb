4dbe87d45873e89c9b523b163a86a47456f5e33d
RMKaelBR <rmbresurreccion@gmail.com>
2023-10-10 21:01:45 +0800

    protect user actions with auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,15 @@ class User < ApplicationRecord
       self.add_role(:student) if self.roles.blank?
     end
   end
+
+  validate :must_have_a_role, on: :update
+
+  private
+  def must_have_a_role
+    puts "must_have_a_role validation called"
+    unless roles.any?
+      errors.add(:roles, "must have at least one role")
+      puts "added some shit"
+    end
+  end
 end
